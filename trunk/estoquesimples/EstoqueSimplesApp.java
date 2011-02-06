9e2243016b07c2f4955d0ec9fdff2de8c137298a
vitorhmr <vitorhmr@ff9af657-307f-69d7-86ee-4254de13bb83>
2011-02-06 12:53:17 +0000

    Foi preciso criar primeiro a tela principal, estou tentando criar a tela de controle de estoque e depois de novo item

---
+++ trunk/estoquesimples/EstoqueSimplesApp.java
@@ -0,0 +1,44 @@
+/*
+ * EstoqueSimplesApp.java
+ */
+
+package estoquesimples;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class EstoqueSimplesApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new EstoqueSimplesView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of EstoqueSimplesApp
+     */
+    public static EstoqueSimplesApp getApplication() {
+        return Application.getInstance(EstoqueSimplesApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(EstoqueSimplesApp.class, args);
+    }
+}
